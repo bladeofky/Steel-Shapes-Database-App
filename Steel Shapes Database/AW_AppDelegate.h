92c68c9ef1d86ef20e09dfe39c9e696d03dcc489
bladeofky <alanwang100@gmail.com>
2014-05-15 22:01:37 -0400

    Initial Commit

---
+++ Steel Shapes Database/AW_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AW_AppDelegate.h
+//  Steel Shapes Database
+//
+//  Created by Alan Wang on 5/15/14.
+//  Copyright (c) 2014 Alan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AW_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Steel Shapes Database/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Steel Shapes Database
+//
+//  Created by Alan Wang on 5/15/14.
+//  Copyright (c) 2014 Alan Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AW_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AW_AppDelegate class]));
+    }
+}
